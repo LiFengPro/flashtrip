2ad409a6cd801e35919d840268b71ee9855de65e
申馨 <shenxin@shenxindeMacBook-Pro.local>
2017-03-20 18:19:12 -0700

    add log  developer

--- frontier/views/home.py
+++ frontier/views/home.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
@@ -7,10 +9,16 @@ from hotel.models.hotel import Hotel
 from hotel.models.quotedprice import QuotedPrice
 from hotel.models.room import Room
 
+logger_test = logging.getLogger("trazip.frontier.test")
 
 def home(request):
     try:
-        city_1 = City.objects.get(pk=1)
+        city_1 = City.objects.get(ctrip_id=1)
+        print("cccccccccccccccccccc")
+        logger_test.info('aaaaaaaaaaaaaaa')
+        logger_test.error('bbbbbbbb')
+        #import pdb; pdb.set_trace()
+        #print("city name is {}, id is {}".format(city_1.name, city_1.id))
     except City.DoesNotExist:
         raise Http404("Cannot find city where pk=1.")
     context = {
